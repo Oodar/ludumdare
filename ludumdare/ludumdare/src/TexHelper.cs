46693777bf32b77284b911e52f821ca7e1287c14
Matthew <matthew.hewitson@btinternet.com>
2011-08-20 22:10:06 +0100

    TexHelper's Tex2DArray now uses sourceRect correctly

--- ludumdare/ludumdare/src/TexHelper.cs
+++ ludumdare/ludumdare/src/TexHelper.cs
@@ -16,17 +16,20 @@ namespace ludumdare.src
         public static Color[,] Tex2DArray(Texture2D tex, Nullable<Rectangle> sourceRect )
         {
             // Create 1D array to hold all Color values in texture
-            Color[] tex1D = new Color[tex.Width * tex.Height];
+            // Calculate proper 1D size
+            int tex1DSize = sourceRect.Value.Width * sourceRect.Value.Height;
+
+            Color[] tex1D = new Color[tex1DSize];
             tex.GetData<Color>(0, sourceRect, tex1D, 0, tex1D.Length);
 
             // 2D texture array
-            Color[,] tex2D = new Color[tex.Width, tex.Height];
+            Color[,] tex2D = new Color[sourceRect.Value.Width, sourceRect.Value.Height];
 
-            for (int x = 0; x < tex.Width; x++)
+            for (int x = 0; x < sourceRect.Value.Width; x++)
             {
-                for (int y = 0; y < tex.Height; y++)
+                for (int y = 0; y < sourceRect.Value.Height; y++)
                 {
-                    tex2D[x, y] = tex1D[x + y * tex.Width];
+                    tex2D[x, y] = tex1D[x + y * sourceRect.Value.Width];
                 }
             }
 
