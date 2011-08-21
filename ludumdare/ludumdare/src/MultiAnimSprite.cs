93ce8f3b9c15425955090457c80341b033ede2ac
Matthew <matthew.hewitson@btinternet.com>
2011-08-21 17:45:54 +0100

    Initial commit

---
+++ ludumdare/ludumdare/src/MultiAnimSprite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace ludumdare.src
+{
+    class MultiAnimSprite
+    {
+        public MultiAnimSprite()
+        {
+           
+
+
+        }
+
+    }
+}
