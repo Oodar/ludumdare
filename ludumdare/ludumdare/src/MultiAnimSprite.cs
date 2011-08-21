21d6a33b6ea7e286991d220068c45b7fe88cad5e
Matthew <matthew.hewitson@btinternet.com>
2011-08-21 20:05:33 +0100

    Functionality added to MultiAnimSprite
    Now can store and play back SpriteBases in Dictionary with a string
    key for the animation name.

--- ludumdare/ludumdare/src/MultiAnimSprite.cs
+++ ludumdare/ludumdare/src/MultiAnimSprite.cs
@@ -12,12 +12,60 @@ namespace ludumdare.src
 {
     class MultiAnimSprite
     {
-        public MultiAnimSprite()
+
+        // Dictionary used to look up for animations
+        Dictionary<string, SpriteBase> m_SpriteAnims;
+
+        // Use to hold current animation
+        string m_strCurrAnim;
+
+        // Current position - MUST SYNC ALL ANIMS TO THIS
+        Vector2 m_vecPos;
+
+        #region Accessors
+        public string CurrentAnimation
         {
-           
-
-
+            get { return m_strCurrAnim;  }
         }
 
+        #endregion
+        
+        public MultiAnimSprite( Vector2 pos )
+        {
+            m_SpriteAnims = new Dictionary<string, SpriteBase>();
+            m_strCurrAnim = "NO_ANIMS_LOADED";
+
+            m_vecPos = pos;
+        }
+
+        public void AddAnimation(string animName, SpriteBase sprite)
+        {
+            m_SpriteAnims.Add(animName, sprite);
+        }
+
+        public void PlayAnimation(string animName)
+        {
+            if (m_SpriteAnims.ContainsKey(animName))
+            {
+                m_strCurrAnim = animName;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!(m_strCurrAnim.Equals("NO_ANIMS_LOADED")))
+            {
+                m_SpriteAnims[m_strCurrAnim].Update(gameTime);
+            }
+        }
+
+        public void Draw( SpriteBatch spriteBatch )
+        {
+            if (!(m_strCurrAnim.Equals("NO_ANIMS_LOADED")))
+            {
+                m_SpriteAnims[m_strCurrAnim].Draw(spriteBatch, m_vecPos);
+            }
+
+        }
     }
 }
